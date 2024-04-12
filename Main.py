aca787ee4593be6a4682def154360efc537e9768
Hemachandra151 <103950858+Hemachandra151@users.noreply.github.com>
2024-04-12 20:34:21 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.title("Math techniques")
+st.subheader("The Available Techniques are: ")
+st.write("***1. Curve Fitting***")
+st.write("***2. Construction Of Polynomial***")
+st.write("***3. Interpolation Of Equal Intervals***")
+st.write("***4. Interpolation Of Equal Intervals***")
+st.write("***5. Linear Regression***")
+st.write("***6. Multiple Regression***")
+st.write("***7. Numerical Differentiation***")
+st.write("***8. Numerical Integration***")
+
+st.write("")
+st.write("")
+st.write("")
+st.subheader("Developed by:")
+st.write("***1. Hemachandra, CSE***")
+
+st.write("")
+st.write("")
+st.write("")
+st.subheader("Special Thanks To:")
+st.write("***1. Dr. M M Praveena***")
+st.write("***2. Dr. Srikantha N***")
